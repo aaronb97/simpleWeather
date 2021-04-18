92a53d13811b3aa1e4dde168e188f0d80b93d113
aaronb97 <aaron9becker7@gmail.com>
2021-04-18 14:42:35 -0400

    add endpoint to get weather based on location string

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,29 @@
 import express from "express";
+import axios from "axios";
+require("dotenv").config();
+require("source-map-support").install();
 
-var app = express();
+const app = express();
 
-app.get("/", (_, res) => {
-  res.send("Hello world!");
+app.get("/q/:q", async (req, res) => {
+  try {
+    const latLonResponse = await axios.get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${req.params.q}&appid=${process.env.WEATHER_KEY}`
+    );
+
+    const { lat, lon } = latLonResponse.data[0];
+
+    const weatherResponse = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_KEY}`
+    );
+
+    res.send(weatherResponse.data);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    // idk
+  }
 });
 
 app.listen(3000);
+console.log("listening on 3000");
